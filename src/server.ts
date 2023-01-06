8d1da7daa4abae781764a6f89e23b2af51e026e7
MD SAIFUR RAHMAN <saifurnstuiit223344@gmail.com>
2023-01-06 10:40:45 +0600

    mongodb connection successful

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,25 @@
 import express, { Application, Request, Response } from "express";
 import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
 import userRoute from "./routes/userRoute";
+import mongoose from "mongoose";
+import { config } from "./config/config";
 
 const app: Application = express();
+const port: number | string = process.env.PORT || 5000;
 
-const PORT: number = 4000;
+// connect to mongodb
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(config.mongo.url, { retryWrites: true, w: "majority" })
+  .then(() => {
+    console.log("MongoDB is Connected");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -16,6 +31,6 @@ app.get("/", (req: Request, res: Response): void => {
 //Routes
 app.use("/api/v1", userRoute);
 
-app.listen(PORT, (): void => {
-  console.log("SERVER IS UP ON PORT:", PORT);
+app.listen(port, (): void => {
+  console.log("SERVER IS UP ON PORT:", port);
 });
